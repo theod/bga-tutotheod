65f12ec8b908858bba9becefbaf3526e10820a3a
Théo de la Hogue <theo.de-la-hogue@enac.fr>
2024-11-24 21:38:36 +0100

    Fixing SQL request.

--- modules/php/Game.php
+++ modules/php/Game.php
@@ -166,7 +166,7 @@ class Game extends \Table
         // Share active player moved token
         return [
             "tokens" => $this->getCollectionFromDb(
-                            "SELECT `token_color` `color`, `square_id` `square`, `slot_id` `slot`, `last_square_id` `last` FROM `tokens` WHERE token_color = player_color AND square_id != last_square_id"
+                            "SELECT `token_color` `color`, `square_id` `square`, `slot_id` `slot`, `last_square_id` `last` FROM `tokens` WHERE token_color = '$player_color' AND square_id != last_square_id"
                         )
         ];
     }
